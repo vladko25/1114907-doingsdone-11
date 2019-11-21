54aad4195f945fef9bec0105f93e3b27ee4d076f
Vlad Koronchik <vladislavkoronchik@gmail.com>
2019-11-21 23:11:17 +0300

    Show the data from the database on the main page

--- index.php
+++ index.php
@@ -6,13 +6,15 @@ date_default_timezone_set("Europe/Moscow");
 $page_content = include_template("main.php", [
     'show_complete_tasks' => $show_complete_tasks,
     'array_projects' => $array_projects,
-    'array_tasks' => $array_tasks
+    'array_tasks' => $array_tasks,
+    'tasks_count' => $tasks_count
     ]
 );
 
 $layout_content = include_template('layout.php', [
     'content' => $page_content,
-    'title' => 'Дела в порядке'
+    'title' => 'Дела в порядке',
+    'cur_user_name' => $cur_user_name['user_name']
     ]
 );
 
