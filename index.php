7b043b3abce49598b2db8c214c22fe9fd40de22a
Vlad Koronchik <vladislavkoronchik@gmail.com>
2019-11-25 04:20:58 +0300

    Построил логику работы с параметром запроса и добавил код ошибки 404 при недопустимом параметре

--- index.php
+++ index.php
@@ -1,23 +1,70 @@
 <?php
-require_once('functions.php');
 require_once('data.php');
-date_default_timezone_set("Europe/Moscow");
 
-$page_content = include_template("main.php", [
-    'show_complete_tasks' => $show_complete_tasks,
-    'array_projects' => $array_projects,
-    'array_tasks' => $array_tasks,
-    'tasks_count' => $tasks_count
-    ]
-);
+if (!isset($_GET['id'])) {
+	$page_content = include_template('main.php', [
+	    'show_complete_tasks' => $show_complete_tasks,
+	    'array_projects' => $array_projects,
+	    'array_tasks' => $array_tasks
+	    ]
+	);
 
-$layout_content = include_template('layout.php', [
-    'content' => $page_content,
-    'title' => 'Дела в порядке',
-    'cur_user_name' => $cur_user_name['user_name']
-    ]
-);
+	$layout_content = include_template('layout.php', [
+	    'content' => $page_content,
+	    'title' => 'Дела в порядке',
+	    'cur_user_name' => $cur_user_name['user_name']
+	    ]
+	);
 
-print($layout_content);
+	print($layout_content);
+}
+else {
+	$id = $_GET['id'];
+
+	$cur_tasks = getRows($con,
+		"SELECT * FROM tasks t 
+		JOIN projects p ON t.project_id = p.id
+		WHERE p.user_id = '$cur_user_id' AND p.id = '$id'"
+	);
+	
+	if(!count($cur_tasks)) {
+		http_response_code(404);
+		$error = "Ошибка 404. Страница не найдена";
+
+		$page_content = include_template('main.php', [
+	    	'show_complete_tasks' => $show_complete_tasks,
+	    	'array_projects' => $array_projects,
+	    	'array_tasks' => $cur_tasks,
+	    	'error' => $error
+	    	]
+	    );
+
+	    $layout_content = include_template('layout.php', [
+	    	'content' => $page_content,
+	    	'title' => 'Дела в порядке',
+	    	'cur_user_name' => $cur_user_name['user_name']
+	    	]
+	    );
+
+	    print($layout_content);
+	}
+	else {
+		$page_content = include_template('main.php', [
+	    	'show_complete_tasks' => $show_complete_tasks,
+	    	'array_projects' => $array_projects,
+	    	'array_tasks' => $cur_tasks
+	    	]
+	    );
+
+		$layout_content = include_template('layout.php', [
+	    	'content' => $page_content,
+	    	'title' => 'Дела в порядке',
+	    	'cur_user_name' => $cur_user_name['user_name']
+	    	]
+	    );
+
+	    print($layout_content);
+	}
+}
 
 ?>
