cd9058906cf305d9809843aa9640fef6ad2351c9
Vlad Koronchik <vladislavkoronchik@gmail.com>
2019-11-25 04:09:30 +0300

    Спрятал запросы в функции

--- data.php
+++ data.php
@@ -1,7 +1,9 @@
 <?php
-// показывать или нет выполненные задачи
-$show_complete_tasks = rand(0, 1);
-$cur_user_id = rand(1, 3);
+require_once('functions.php');
+date_default_timezone_set("Europe/Moscow");
+
+$show_complete_tasks = 1;
+$cur_user_id = 1;
 $cur_user_name = '';
 $array_projects = [];
 $array_tasks = [];
@@ -15,26 +17,18 @@ if (!$con) {
     $error = mysqli_connect_error();
 }
 else {
-    $sql = "SELECT user_name FROM users WHERE id = '$cur_user_id'";
-    $result = mysqli_query($con, $sql);
-    if ($result) {
-        $cur_user_name = mysqli_fetch_assoc($result);
-    }
-
-    $sql = "SELECT p.project_name, COUNT(t.id) AS tasks_count FROM projects p
-    JOIN tasks t ON p.id = t.project_id
-    WHERE t.user_id = '$cur_user_id' AND t.project_id = p.id
-    GROUP BY project_id";
-    $result = mysqli_query($con, $sql);
-    if ($result) {
-        $array_projects = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    }
-
-    $sql = "SELECT t.task_name, p.project_name, DATE_FORMAT(deadline, '%d.%m.%Y') AS deadline, status FROM tasks AS t
-    JOIN projects AS p USING(user_id)
-    WHERE t.user_id = '$cur_user_id' AND t.project_id = p.id";
-    $result = mysqli_query($con, $sql);
-    if ($result) {
-        $array_tasks = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    }
+    $cur_user_name = getRow($con, "SELECT user_name FROM users WHERE id = '$cur_user_id'");
+    $array_projects = getRows($con,
+        "SELECT p.id, p.project_name, COUNT(t.id) AS tasks_count
+        FROM projects p
+        LEFT JOIN tasks t ON p.id = t.project_id
+        WHERE t.user_id = '$cur_user_id'
+        GROUP BY project_id"
+    );
+    $array_tasks = getRows($con,
+        "SELECT t.task_name, p.project_name, DATE_FORMAT(deadline, '%d.%m.%Y') AS deadline, status
+        FROM tasks AS t
+        JOIN projects AS p USING(user_id)
+        WHERE t.user_id = '$cur_user_id' AND t.project_id = p.id"
+    );
 }
