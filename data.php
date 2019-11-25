ead5ba9147e94bf8ca4e492262f2bc0bb349c470
Vlad Koronchik <vladislavkoronchik@gmail.com>
2019-11-26 02:46:37 +0300

    Почистил код и исправил документацию

--- data.php
+++ data.php
@@ -2,33 +2,23 @@
 require_once('functions.php');
 date_default_timezone_set("Europe/Moscow");
 
-$show_complete_tasks = 1;
+$show_complete_tasks = rand(0, 1);
 $cur_user_id = 1;
 $cur_user_name = '';
 $array_projects = [];
 $array_tasks = [];
-$tasks_count = 0;
 $error = '';
 
 $con = mysqli_connect("localhost", "root", "","doingsdone");
 mysqli_set_charset($con, "utf8");
 
-if (!$con) {
+if (!$con)
+{
     $error = mysqli_connect_error();
 }
-else {
-    $cur_user_name = getRow($con, "SELECT user_name FROM users WHERE id = '$cur_user_id'");
-    $array_projects = getRows($con,
-        "SELECT p.id, p.project_name, COUNT(t.id) AS tasks_count
-        FROM projects p
-        LEFT JOIN tasks t ON p.id = t.project_id
-        WHERE t.user_id = '$cur_user_id'
-        GROUP BY project_id"
-    );
-    $array_tasks = getRows($con,
-        "SELECT t.task_name, p.project_name, DATE_FORMAT(deadline, '%d.%m.%Y') AS deadline, status
-        FROM tasks AS t
-        JOIN projects AS p USING(user_id)
-        WHERE t.user_id = '$cur_user_id' AND t.project_id = p.id"
-    );
+else
+{
+    $array_projects = getProjectsOfUser($con, $cur_user_id);
+    $array_tasks = getTasksOfUser($con, $cur_user_id);
+    $cur_user_name = $array_tasks[0]['user_name'];
 }
--- index.php
+++ index.php
@@ -1,70 +1,22 @@
 <?php
 require_once('data.php');
 
-if (!isset($_GET['id'])) {
-	$page_content = include_template('main.php', [
-	    'show_complete_tasks' => $show_complete_tasks,
-	    'array_projects' => $array_projects,
-	    'array_tasks' => $array_tasks
-	    ]
-	);
-
-	$layout_content = include_template('layout.php', [
-	    'content' => $page_content,
-	    'title' => 'Дела в порядке',
-	    'cur_user_name' => $cur_user_name['user_name']
-	    ]
-	);
-
-	print($layout_content);
+if (!isset($_GET['id']))
+{
+	getContent($show_complete_tasks, $array_projects, $array_tasks, $cur_user_name);
 }
-else {
+else
+{
 	$id = $_GET['id'];
+	$cur_tasks = getTasksOfActiveProject($con, $cur_user_id, $id);
 
-	$cur_tasks = getRows($con,
-		"SELECT * FROM tasks t 
-		JOIN projects p ON t.project_id = p.id
-		WHERE p.user_id = '$cur_user_id' AND p.id = '$id'"
-	);
-	
-	if(!count($cur_tasks)) {
+	if(!count($cur_tasks))
+	{
 		http_response_code(404);
 		$error = "Ошибка 404. Страница не найдена";
-
-		$page_content = include_template('main.php', [
-	    	'show_complete_tasks' => $show_complete_tasks,
-	    	'array_projects' => $array_projects,
-	    	'array_tasks' => $cur_tasks,
-	    	'error' => $error
-	    	]
-	    );
-
-	    $layout_content = include_template('layout.php', [
-	    	'content' => $page_content,
-	    	'title' => 'Дела в порядке',
-	    	'cur_user_name' => $cur_user_name['user_name']
-	    	]
-	    );
-
-	    print($layout_content);
 	}
-	else {
-		$page_content = include_template('main.php', [
-	    	'show_complete_tasks' => $show_complete_tasks,
-	    	'array_projects' => $array_projects,
-	    	'array_tasks' => $cur_tasks
-	    	]
-	    );
 
-		$layout_content = include_template('layout.php', [
-	    	'content' => $page_content,
-	    	'title' => 'Дела в порядке',
-	    	'cur_user_name' => $cur_user_name['user_name']
-	    	]
-	    );
-
-	    print($layout_content);
-	}
+	getContent($show_complete_tasks, $array_projects, $cur_tasks, $cur_user_name, $error);
 }
 
 ?>
